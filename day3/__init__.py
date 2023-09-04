0988231ad1d371c50dc09da2ab275ff10abf0b16
souleiman2 <souleiman_ayoub@hotmail.com>
2023-09-04 13:58:46 -0400

    finished day3

---
+++ day3/__init__.py
@@ -0,0 +1,38 @@
+def read_txt_file(filename):
+    f = open(filename + ".txt", "r")
+    return f.readlines()
+
+def strip(line):
+    return line.strip()
+
+def find_priority(letter):
+    if ord("a") <= ord(letter) <= ord("z"):
+        return ord(letter) - ord("a") + 1
+    return ord(letter) - ord("A") + 27
+
+def find_similar(first_string, second_string):
+    first_letters = sorted(first_string)
+    second_letters = sorted(second_string)
+    
+    index_first, index_second = 0, 0
+    while index_first < len(first_letters) and index_second < len(second_letters):
+        if first_letters[index_first] == second_letters[index_second]:
+            return first_letters[index_first]
+        elif first_letters[index_first] < second_letters[index_second]:
+            index_first += 1
+        else:
+            index_second += 1
+    raise ValueError("There seems to be no similar value in both containers")
+
+def calc_score_line(line):
+    first_container, second_container = line[:len(line)//2], line[len(line)//2:]
+    similar_letter = find_similar(first_container, second_container)
+    return find_priority(similar_letter)
+
+if __name__ == "__main__":
+    lines = read_txt_file("input")
+    lines = list(map(strip, lines))
+    scores = list(map(calc_score_line, lines))
+    print(f"Here is the sum of all the priorities : {sum(scores)}")
+    
+    
---
+++ day3/part2.py
@@ -0,0 +1,44 @@
+def read_txt_file(filename):
+    f = open(filename + ".txt", "r")
+    return f.readlines()
+
+def strip(line):
+    return line.strip()
+
+def find_priority(letter):
+    if ord("a") <= ord(letter) <= ord("z"):
+        return ord(letter) - ord("a") + 1
+    return ord(letter) - ord("A") + 27
+
+def find_similar(first_string, second_string):
+    first_letters = sorted(first_string)
+    second_letters = sorted(second_string)
+    
+    similar = set()
+    index_first, index_second = 0, 0
+    while index_first < len(first_letters) and index_second < len(second_letters):
+        if first_letters[index_first] == second_letters[index_second]:
+            similar.add(first_letters[index_first])
+            index_first += 1
+            index_second += 1
+        elif first_letters[index_first] < second_letters[index_second]:
+            index_first += 1
+        else:
+            index_second += 1
+    return ''.join(similar)
+
+def calc_group_lines(lines):
+    current_line = lines[0]
+    for i in range(1, len(lines)):
+        current_line = find_similar(current_line, lines[i])
+    return find_priority(current_line)
+
+def group_lines(lines, group_size = 3):
+    return [lines[i*group_size:(i+1)*group_size] for i in range(len(lines)//group_size)]
+
+if __name__ == "__main__":
+    lines = read_txt_file("input")
+    lines = list(map(strip, lines))
+    grouped_lines = group_lines(lines)
+    scores = list(map(calc_group_lines, grouped_lines))
+    print(f"Here is the sum of all the priorities : {sum(scores)}")
